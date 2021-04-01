f1301f454499045f9f2facec4a1802a7598e736d
Ingrid Crant <55631417+ingridcrant@users.noreply.github.com>
2021-04-01 19:36:54 -0400

    improved debug of Coin.java

--- Coin.java
+++ Coin.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Coin extends Rectangle {
     private static final BufferedImage coinSpriteImage = JetpackJoyridePanel.loadBuffImg("coinsprite.png");
     private static final int NUMSPRITES = 6;
+    public static final int WIDTH = coinSpriteImage.getWidth()/NUMSPRITES, HEIGHT = coinSpriteImage.getHeight(), GAP = WIDTH+2;
     private static final BufferedImage[] sprites = getSprites(coinSpriteImage, NUMSPRITES);
     private int currentSprite, x, y;
-    public static int WIDTH, HEIGHT, GAP;
     
     private static boolean isRotating = true;   // for if the coin is rotating or not
 
@@ -19,22 +19,16 @@ public class Coin extends Rectangle {
         y = yy;
         currentSprite = rand.nextInt(NUMSPRITES);
     }
-
-    // Getter and setter methods:
-    public BufferedImage getImage() {
-        return sprites[currentSprite];
-    }
     private static BufferedImage[] getSprites(BufferedImage spriteSheet, int numSprites) {
         BufferedImage[] sprites = new BufferedImage[numSprites];
-        WIDTH = coinSpriteImage.getWidth()/6;
-        HEIGHT = coinSpriteImage.getHeight();
-        GAP = WIDTH+2;
-
         for(int i = 0; i < numSprites; i++) {
             sprites[i] = spriteSheet.getSubimage(i*WIDTH, 0, WIDTH, HEIGHT);
         }
-
         return sprites;
+    }
+    // Getter and setter methods:
+    public BufferedImage getImage() {
+        return sprites[currentSprite];
     }
     public static void stopRotating() {
         isRotating = false;
